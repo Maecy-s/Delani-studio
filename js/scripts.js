78daef7d6252aa6a69239c57883816882e13a2d5
Maecy-s <mercy.chepkemboi@student.moringaschool.com>
2021-06-19 08:59:45 +0300

    Add new changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
+
+$(document).ready(function(){
+
+
 $(document).ready(function(){
     $("button").click(function(){
         $("div.des").toggle();
     });
+});
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("div.deve").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("div.pro").toggle();
+    });
+});
 });
