9fe84b3adffa1e1997499c16a9af602fe3ed863b
Maecy-s <mercy.chepkemboi@student.moringaschool.com>
2021-06-19 13:28:50 +0300

    Add new js, html and css changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,52 @@
 
-$(document).ready(function(){
+function myFunction() {
+    var x = document.getElementById("des");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none"
+    }
+}
 
 
-$(document).ready(function(){
-    $("button").click(function(){
-        $("div.des").toggle();
-    });
-});
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("div.des").toggle();
+//     });
+// });
 
-$(document).ready(function(){
-    $("button").click(function(){
-        $("div.deve").toggle();
-    });
-});
+function meFunction() {
+    var y = document.getElementById("deve");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    }
+    else {
+        y.style.display = "none"
+    }
+}
 
-$(document).ready(function(){
-    $("button").click(function(){
-        $("div.pro").toggle();
-    });
-});
-});
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("div.deve").toggle();
+//     });
+// });
+
+
+function minFunction() {
+    var z = document.getElementById("pro");
+    if (z.style.display === "none") {
+        z.style.display = "block";
+    }
+    else {
+        z.style.display = "none"
+    }
+}
+
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("div.pro").toggle();
+//     });
+// });
