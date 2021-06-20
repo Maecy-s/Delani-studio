bfddff0de4ec25571d7692d00232ef3d698a05d0
Maecy-s <mercy.chepkemboi@student.moringaschool.com>
2021-06-20 18:04:16 +0300

    Add new changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 
-function myFunction() {
-    var x = document.getElementById("des");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    }
-    else {
-        x.style.display = "none"
-    }
-}
+// function myFunction() {
+//     var x = document.getElementById("des");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     }
+//     else {
+//         x.style.display = "none"
+//     }
+// }
 
 
 // $(document).ready(function(){
@@ -16,15 +16,15 @@ function myFunction() {
 //     });
 // });
 
-function meFunction() {
-    var y = document.getElementById("deve");
-    if (y.style.display === "none") {
-        y.style.display = "block";
-    }
-    else {
-        y.style.display = "none"
-    }
-}
+// function meFunction() {
+//     var y = document.getElementById("deve");
+//     if (y.style.display === "none") {
+//         y.style.display = "block";
+//     }
+//     else {
+//         y.style.display = "none"
+//     }
+// }
 
 
 // $(document).ready(function(){
@@ -34,15 +34,15 @@ function meFunction() {
 // });
 
 
-function minFunction() {
-    var z = document.getElementById("pro");
-    if (z.style.display === "none") {
-        z.style.display = "block";
-    }
-    else {
-        z.style.display = "none"
-    }
-}
+// function minFunction() {
+//     var z = document.getElementById("pro");
+//     if (z.style.display === "none") {
+//         z.style.display = "block";
+//     }
+//     else {
+//         z.style.display = "none"
+//     }
+// }
 
 
 // $(document).ready(function(){
@@ -50,3 +50,25 @@ function minFunction() {
 //         $("div.pro").toggle();
 //     });
 // });
+
+
+
+$(document).ready(function(){
+    $(".main").click(function(){
+        $(this).children("#paragraph").slideToggle(1000);
+        $(this).children("#design").toggle();
+    });
+    $(".project").mouseover(function(){
+        $(this).children(".port").show();
+        
+    })
+     .mouseout(function(){
+        $(this).children(".port").hide();
+    })
+    $("#submitForm").click(function(){
+        var naMe = document.getElementById("name").value;
+        alert("Hi " + naMe + " " + "we have received your message. Thank you for reaching out to us.");
+    });
+});
+
+
